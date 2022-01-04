cc9b415005b0c81321f215f5743e5dcf6e2e7286
jayraikhere <jayraykhere@gmail.com>
2022-01-04 21:29:41 +0530

    added getrooms

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { createRoom, patchRoom, delRoom } from '../controllers/roomController.js';
+import { createRoom, patchRoom, delRoom, getRooms } from '../controllers/roomController.js';
 import { isLoggedIn } from '../middlewares/validity.js';
 
 const router = express.Router();
 
+router.get('/',isLoggedIn, getRooms);
 router.post('/create', isLoggedIn, createRoom);
 router.patch('/:roomId', isLoggedIn, patchRoom);
 router.delete('/:roomId', isLoggedIn, delRoom);
