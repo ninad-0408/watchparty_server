29f3bf7389136fc754038226904db84c391c8945
Ninad Kalanke <ninadkalanke@gmail.com>
2022-01-09 17:26:34 +0530

    error fixed

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -4,7 +4,7 @@ const handleSocket = (io, socket) => {
 
     socket.on('new-member', (room) => {
         users.push({ _id: socket.user._id, username: socket.user.username, isAdmin: false, isHost: false });
-        socket.emit('member-connected', users);
+        io.emit('member-connected', users);
     });
 
     socket.on('message', (message) => {
@@ -13,7 +13,7 @@ const handleSocket = (io, socket) => {
 
     socket.on('disconnect', () => {
         users = users.filter((e) => e._id !== socket.user._id ? e : null);
-        socket.emit('member-connected', users);
+        io.emit('member-connected', users);
     })
 
 };
