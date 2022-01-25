971eca26a7fcf785de9115122a1a5e11b5776bde
Ninad Kalanke <ninadkalanke@gmail.com>
2022-01-25 15:00:14 +0530

    CORS error try 1

--- index.js
+++ index.js
@@ -15,23 +15,33 @@ import cookieParser from "cookie-parser";
 import videoSearchRoute from "./routes/videoSearchRoute.js";
 
 const app = express();
+app.all("/", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(cookieParser());
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+  },
 });
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors( { origin: [ "http://localhost:3000", "https://watch-party-project.web.app" ], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://watch-party-project.web.app"],
+    credentials: true,
+  })
+);
 app.use(auth);
 
 app.use("/user", userRoute);
 app.use("/room", roomRoute);
 app.post("/videoSearch", videoSearchRoute);
-app.use(( err, req, res, next) => errorHandler);
+app.use((err, req, res, next) => errorHandler);
 
 io.use(socketAuth);
 io.on("connection", (socket) => handleSocket(io, socket));
@@ -39,13 +49,13 @@ io.on("connection", (socket) => handleSocket(io, socket));
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(process.env.CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() =>
-        server.listen(PORT, () =>
-            console.log(`The server is running on port: ${PORT}`)
-        )
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    server.listen(PORT, () =>
+      console.log(`The server is running on port: ${PORT}`)
     )
-    .catch((error) => console.log(error.message));
+  )
+  .catch((error) => console.log(error.message));
