2139cb4c0866cc7ace3a3096f46f801f776466e5
Ninad Kalanke <ninadkalanke@gmail.com>
2022-01-25 15:12:14 +0530

    CORS error try 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import videoSearchRoute from "./routes/videoSearchRoute.js";
 const app = express();
 app.all("/", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(cookieParser());
