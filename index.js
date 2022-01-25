3105cea3547247c52a87cdc06cdb9353454b9740
Ninad Kalanke <ninadkalanke@gmail.com>
2022-01-25 14:35:13 +0530

    Cookies from frontend

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors( { origin: ["https://watch-party-project.web.app"], credentials: true }));
+app.use(cors( { origin: [ "http://localhost:3000", "https://watch-party-project.web.app" ], credentials: true }));
 app.use(auth);
 
 app.use("/user", userRoute);
